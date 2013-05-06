d919023daf775c241bf5d796d583585e0a19f297
Ken Seals <kenseals@gmail.com>
2013-05-06 15:15:01 -0500

    removed unused stuff

--- js/main.js
+++ js/main.js
@@ -1,63 +1,7 @@
 jQuery(function($){
 	
-	// var element = $('.content-pane');
-//     var hammertime = Hammer(element).on("swipeleft", function(event) {
-//     	console.log();
-//     });
-
-	
-	
-	// ev.gesture.preventDefault();
-	
+	// Init sideNav
 	sideNav();
-	
-	
-		// 
-	// var hammertime = Hammer(document.body);
-	// 
-	//     hammertime.on('tap', function(ev) {
-	// 	console.log('hey');
-	//     });
-	
-
-	// function manageMultitouch(ev){
-	//  
-	//         switch(ev.type) {
-	//             case 'touch':
-	//                 last_scale = scale;
-	//                 last_rotation = rotation;
-	// 
-	//                 break;
-	// 
-	//             case 'drag':
-	//                     posX = ev.gesture.deltaX + lastPosX;
-	//                     posY = ev.gesture.deltaY + lastPosY;
-	//                 break;
-	// 
-	//             case 'transform':
-	//                 rotation = last_rotation + ev.gesture.rotation;
-	//                 scale = Math.max(1, Math.min(last_scale * ev.gesture.scale, 10));
-	//                 break;
-	// 
-	//             case 'dragend':
-	//                 lastPosX = posX;
-	//                 lastPosY = posY;
-	//                 break;
-	//         }
-	// 
-	//         var transform =
-	//         "translate3d("+posX+"px,"+posY+"px, 0) " +
-	//         "scale3d("+scale+","+scale+", 0) " +
-	//         "rotate("+rotation+"deg) ";
-	// 
-	//         elemRect.style.transform = transform;
-	//         elemRect.style.oTransform = transform;
-	//         elemRect.style.msTransform = transform;
-	//         elemRect.style.mozTransform = transform;
-	//         elemRect.style.webkitTransform = transform;
-	//     }
-	// 	
-	// 	// console.log(posX);
 
 });
 
