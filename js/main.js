065293a35e00ef5da7b00d328cb9d618c79327d7
Ken Seals <kenseals@gmail.com>
2013-05-09 20:49:33 -0500

    Added example of calling sideNav public method from external func. Also, plugin cleanup.

--- js/main.js
+++ js/main.js
@@ -3,223 +3,227 @@ $(document).ready(function(){
 	// Init sideNav
 	$('body').sideNav();
 	
+	// Save so its public methods are accessible
+	var sideNav = $('body').data('sideNav');
+	
+	// Example click event calling sideNav public method
+	$('#example').add('#nav a').click(function(){
+		sideNav.close();
+		
+		// Make sure not to return href
+		return false;
+	});
+	
 });
 
 
 
 (function($){	
 	var SideNav = function(element, options){
-		    var elem = $(element),
-		    	obj = this;
-			
-			var defaults = {
-				dataAttrs: false,
-				prefix: "side-nav",
-				burger: "burger",
-				nav: "nav",
-				app: "app",
-				content: "content"
-			};
-			
-			var config = $.extend(defaults, options || {});
+	    var elem = $(element),
+	    	obj = this;
 	
-			var state = {
-				open:false
-			};
-			
-			var prefix = "data-"+config.prefix,
-				burger = config.burger,
-				nav = config.nav,
-				app = config.app,
-				content = config.content;
-				
-			if (config.dataAttrs === false) {
-				
-				var $burger = $('#'+burger),
-					$nav = $('#'+nav),
-					$app = $('#'+app),
-					$content = $('#'+content),
-					navWidth = $nav.width();
-			}
-			else {
-				
-				var $burger = $('['+prefix+'="'+burger+'"]'),
-					$nav = $('['+prefix+'="'+nav+'"]'),
-					$app = $('['+prefix+'="'+app+'"]'),
-					$content = $('['+prefix+'="'+content+'"]'),
-					navWidth = $nav.width();
-			}
-				
+		var defaults = {
+			dataAttrs: false,
+			prefix: "side-nav",
+			burger: "burger",
+			nav: "nav",
+			app: "app",
+			content: "content"
+		};
 	
-			this.open = function() {
+		var config = $.extend(defaults, options || {});
+
+		var state = {
+			open:false
+		};
+	
+		var prefix = "data-"+config.prefix,
+			burger = config.burger,
+			nav = config.nav,
+			app = config.app,
+			content = config.content;
 		
-				console.log('open');
+		if (config.dataAttrs === false) {
 		
-				// Add class to body for stying purposes
-				$('body').removeClass('nav-closed').addClass('nav-open');
+			var $burger = $('#'+burger),
+				$nav = $('#'+nav),
+				$app = $('#'+app),
+				$content = $('#'+content),
+				navWidth = $nav.width();
+		}
+		else {
 		
-				// Animate App
-		        if(Modernizr.csstransforms) {
-		            $app.css("transform", "translate("+navWidth+"px,0)");
-					$app.css("-webkit-transition", "0.2s ease-out");
-		        }
-		        else {
-		            $app.css("left",  navWidth+"px");
-		        }
+			var $burger = $('['+prefix+'="'+burger+'"]'),
+				$nav = $('['+prefix+'="'+nav+'"]'),
+				$app = $('['+prefix+'="'+app+'"]'),
+				$content = $('['+prefix+'="'+content+'"]'),
+				navWidth = $nav.width();
+		}
 		
-				// Unbind content tap
-				$content.unbind("tap drag dragend");
+
+		this.open = function() {
+
+			console.log('open');
+
+			// Add class to body for stying purposes
+			$('body').removeClass('nav-closed').addClass('nav-open');
+
+			// Animate App
+	        if(Modernizr.csstransforms) {
+	            $app.css("transform", "translate("+navWidth+"px,0)");
+				$app.css("-webkit-transition", "0.2s ease-out");
+	        }
+	        else {
+	            $app.css("left",  navWidth+"px");
+	        }
+
+			// Unbind content tap
+			$content.unbind("tap drag dragend");
+
+			// Bind content tap
+			$content.hammer().on("tap drag dragend", function(event) {
+	
+				// Detect event type
+				if (event.type === 'tap') {
 		
-				// Bind content tap
-				$content.hammer().on("tap drag dragend", function(event) {
-			
-					// Detect event type
-					if (event.type === 'tap') {
-				
-						console.log('tap!');
-				
-						// Fire touchEvent
-						touchEvent();
-				
-					}
-					if (event.type === 'dragend') {
-						console.log('released');
-				
-						if (Math.abs(event.gesture.deltaX) > navWidth/2) {
-							console.log('more than 50%');
-					
-				            if(Modernizr.csstransforms) {
-				                $app.css("transform", "translate(0px,0)");
-								$app.css("-webkit-transition", "0.2s ease-out");
-				            }
-				            else {
-				                $app.css("left", "0px");
-				            }	
-					
-							// Fire touchEvent
-							touchEvent();
-						} 
-						else {
-				            if(Modernizr.csstransforms) {
-				                $app.css("transform", "translate("+navWidth+"px,0)");
-								$app.css("-webkit-transition", "0.2s ease-out");
-				            }
-				            else {
-				                $app.css("left",  navWidth+"px");
-				            }	
-						}
-				
-					}
-					if (event.type === 'drag') {
-				
-						// disable browser scrolling
-						event.gesture.preventDefault();
-				
-						// stick to the finger
-		                var dragOffset = event.gesture.center.pageX,
-							dragStart = event.gesture.startEvent.center.pageX,
-							offset = dragOffset - (dragStart - navWidth);
-					
-						// Move app container
-		                moveAppContainer(offset);
-				
-						function moveAppContainer(offset) {
-					
-				            if(Modernizr.csstransforms3d) {
-				                $app.css("transform", "translate3d("+offset+"px,0,0)");
-				            }
-							else if (Modernizr.csstransforms) {
-				                $app.css("transform", "translate("+offset+"px,0)");
-							}
-				            else {
-				                $app.css("left", offset+"px");
-				            }
-					
-							$app.css("-webkit-transition", "0s ease-in");
-				        }
-				
-					}
-			
-			
-				});
+					console.log('tap!');
 		
-				// Set state
-				state.open = true;
-			}
-			this.close = function() {
-		
-				console.log('close');
-		
-				// Add class to body for styling purposes
-				$('body').removeClass('nav-open').addClass('nav-closed');
-		
-				// Animate App
-		        if(Modernizr.csstransforms) {
-		            $app.css("transform", "translate(0px,0)");
-					$app.css("-webkit-transition", "0.2s ease-out");
-		        }
-		        else {
-		            $app.css("left", "0px");
-		        }	
-		
-				// Unbind content tap
-				$content.unbind("tap drag dragend");
-		
-				// Set state
-				state.open = false;
-			}
-			this.listen = function() {
-		
-				var hammertime = Hammer(document.body, {
-					drag_lock_to_axis:true
-				});
-		
-				$burger.hammer().on("tap", function(event) {
-			
 					// Fire touchEvent
 					touchEvent();
-					
-				});
 		
-				console.log('listening');
-			}
-	
-			function touchEvent() {
-		
-				// If closed, open nav.
-				if (state.open === false) {
-					console.log('state closed');
-					obj.open();
 				}
+				if (event.type === 'dragend') {
+					console.log('released');
 		
-				// Close nav.
-				else {
-					console.log('state open');
-					obj.close();
-				}	
-			}
+					if (Math.abs(event.gesture.deltaX) > navWidth/2) {
+						console.log('more than 50%');
+			
+			            if(Modernizr.csstransforms) {
+			                $app.css("transform", "translate(0px,0)");
+							$app.css("-webkit-transition", "0.2s ease-out");
+			            }
+			            else {
+			                $app.css("left", "0px");
+			            }	
+			
+						// Fire touchEvent
+						touchEvent();
+					} 
+					else {
+			            if(Modernizr.csstransforms) {
+			                $app.css("transform", "translate("+navWidth+"px,0)");
+							$app.css("-webkit-transition", "0.2s ease-out");
+			            }
+			            else {
+			                $app.css("left",  navWidth+"px");
+			            }	
+					}
+		
+				}
+				if (event.type === 'drag') {
+		
+					// disable browser scrolling
+					event.gesture.preventDefault();
+		
+					// stick to the finger
+	                var dragOffset = event.gesture.center.pageX,
+						dragStart = event.gesture.startEvent.center.pageX,
+						offset = dragOffset - (dragStart - navWidth);
+			
+					// Move app container
+	                moveAppContainer(offset);
+		
+					function moveAppContainer(offset) {
+			
+			            if(Modernizr.csstransforms3d) {
+			                $app.css("transform", "translate3d("+offset+"px,0,0)");
+			            }
+						else if (Modernizr.csstransforms) {
+			                $app.css("transform", "translate("+offset+"px,0)");
+						}
+			            else {
+			                $app.css("left", offset+"px");
+			            }
+			
+						$app.css("-webkit-transition", "0s ease-in");
+			        }
+		
+				}
 	
-			function matrixToArray(matrix) {
-			    return matrix.substr(7, matrix.length - 8).split(', ');
+	
+			});
+
+			// Set state
+			state.open = true;
+		}
+		this.close = function() {
+
+			console.log('close');
+
+			// Add class to body for styling purposes
+			$('body').removeClass('nav-open').addClass('nav-closed');
+
+			// Animate App
+	        if(Modernizr.csstransforms) {
+	            $app.css("transform", "translate(0px,0)");
+				$app.css("-webkit-transition", "0.2s ease-out");
+	        }
+	        else {
+	            $app.css("left", "0px");
+	        }	
+
+			// Unbind content tap
+			$content.unbind("tap drag dragend");
+
+			// Set state
+			state.open = false;
+		}
+		this.listen = function() {
+
+			// Bind tap event to burger button
+			$burger.hammer().on("tap", function(event) {
+	
+				// Fire touchEvent
+				touchEvent();
+			
+			});
+
+			console.log('listening');
+		}
+
+		function touchEvent() {
+
+			// If closed, open nav.
+			if (state.open === false) {
+				console.log('state closed');
+				obj.open();
 			}
-			
-			// Start listening for open event
-			this.listen();
-			
-	  };
 
-	  $.fn.sideNav = function(options){
-	    return this.each(function(){
-	      var element = $(this);
+			// Close nav.
+			else {
+				console.log('state open');
+				obj.close();
+			}	
+		}
+	
+		// Start listening for open event
+		this.listen();
+	
+	};
 
-	      // Return early if this element already has a plugin instance
-	      if (element.data('sideNav')) return;
+	$.fn.sideNav = function(options){
+		return this.each(function(){
+			var element = $(this);
 
-	      // pass options to plugin constructor
-	      var sideNav = new SideNav(this, options);
+			// Return early if this element already has a plugin instance
+			if (element.data('sideNav')) return;
 
-	      // Store plugin object in this element's data
-	      element.data('sideNav', sideNav);
-	    });
-	  };
+			// pass options to plugin constructor
+			var sideNav = new SideNav(this, options);
+
+			// Store plugin object in this element's data
+			element.data('sideNav', sideNav);
+		});
+	};
 })(jQuery);
